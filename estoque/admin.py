51830f6d8a1f2424805cdb8eda77c42093075306
Luan Seslley Reis do Nascimento <luan.nascimento@cosampa.com.br>
2026-08-13 19:23:54 -0300

    teste: remover template customizado do cliente

--- estoque/admin.py
+++ estoque/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.db.models import Count
 from django.template.response import TemplateResponse
 from .models import Cliente, Produto, Venda, ResumoVendas
+from django.db.models import Count, Sum
 
 
 class VendaInline(admin.TabularInline):
@@ -14,7 +15,30 @@ class VendaInline(admin.TabularInline):
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'parceiro', 'ativo')
     inlines = [VendaInline]
+    # change_form_template = "admin/estoque/cliente/change_form.html"
 
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+
+        vendas = Venda.objects.filter(cliente_id=object_id)
+
+        total_pago = vendas.filter(pago=True).aggregate(
+            total=Sum('valor_total')
+        )['total'] or 0
+
+        total_prazo = vendas.filter(pago=False).aggregate(
+            total=Sum('valor_total')
+        )['total'] or 0
+
+        extra_context.update({
+            'total_pago': total_pago,
+            'total_prazo': total_prazo,
+            'saldo': total_prazo - total_pago,
+        })
+
+        return super().change_view(
+            request, object_id, form_url, extra_context=extra_context
+        )
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
@@ -42,12 +66,27 @@ class ResumoVendasAdmin(admin.ModelAdmin):
         return False
 
     def changelist_view(self, request, extra_context=None):
-        total_vendas = Venda.objects.count()
-        vendas_pagas = Venda.objects.filter(pago=True).count()
-        vendas_pendentes = Venda.objects.filter(pago=False).count()
+        vendas = Venda.objects.all()
+
+        cliente_id = request.GET.get('cliente')
+        data_inicio = request.GET.get('data_inicio')
+        data_fim = request.GET.get('data_fim')
+
+        if cliente_id:
+            vendas = vendas.filter(cliente_id=cliente_id)
+
+        if data_inicio:
+            vendas = vendas.filter(data__date__gte=data_inicio)
+
+        if data_fim:
+            vendas = vendas.filter(data__date__lte=data_fim)
+
+        total_vendas = vendas.count()
+        vendas_pagas = vendas.filter(pago=True).count()
+        vendas_pendentes = vendas.filter(pago=False).count()
 
         vendas_por_produto = (
-            Venda.objects.values('produto__nome')
+            vendas.values('produto__nome')
             .annotate(total=Count('id'))
             .order_by('-total')
         )
@@ -59,6 +98,10 @@ class ResumoVendasAdmin(admin.ModelAdmin):
             'vendas_pagas': vendas_pagas,
             'vendas_pendentes': vendas_pendentes,
             'vendas_por_produto': vendas_por_produto,
+            'clientes': Cliente.objects.all().order_by('nome'),
+            'cliente_selecionado': cliente_id,
+            'data_inicio': data_inicio or '',
+            'data_fim': data_fim or '',
         })
 
         return TemplateResponse(
